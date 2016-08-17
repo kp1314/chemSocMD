09755cecc83d5496f5b8370d04ef8acdf51f5456
Kiran Patel <kp1314@ic.ac.uk>
2016-08-17 23:34:17 +0100

    tring to work out all the measures for children and parents

--- findGroupings.py
+++ findGroupings.py
@@ -1,14 +1,36 @@
 import csv
-childReader = csv.reader(open('children.csv', 'rb'))
-parentReader = csv.reader(open('parents.csv', 'rb'))
+import numpy as np
+import pandas as pd  #pandas removes the top row of the csv as its un important
+from sys import argv
 
 def main():
-    sortChildren(childReader, parentReader)
+
+    arg1, arg2, arg3 = argv
+
+    childResults = pd.read_csv(arg2).as_matrix()
+    parentResults = pd.read_csv(arg3).as_matrix()
+
+    ret = sortChildren(childResults, parentResults)
+    print("Sorted innit :P")
     #work out how to print the results of sortChildren
 
 #work out the Differences between the
-def workOutDifference(childRow, parentRow):
+def differenceSum(childCol, parentCol):
+    np.sum(np.square(np.subtract(parentCol, childCol)))
 
 
-def sortChildren():
+def sortChildren(childResults, parentResults):
+    clen = len(childResults)
+    plen = len(parentResults)
+    parentsChildren = []
     leftOvers = []
+    comparisonTable = [[0 for x in range(plen)] for y in range(clen)]
+
+    for i in range(7, clen):
+        for j in range(7, plen):
+            comparisonTable[i][j] = differenceSum(childResults[i], parentResults[j])
+
+    print(comparisonTable)
+
+if __name__ == "__main__":
+    main()
